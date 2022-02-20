e41f92d9d531d74caf654bd138367036cf77a439
riikacharya-code <racharya@vassar.edu>
2022-02-20 17:42:05 -0500

    twitter_ios_starter

---
+++ Twitter/LoginViewController.swift
@@ -0,0 +1,44 @@
+//
+//  LoginViewController.swift
+//  Twitter
+//
+//  Created by Riik Acharya on 2/17/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        if UserDefaults.standard.bool(forKey: "userLoggedIn") == true {
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+        
+    }
+    
+    @IBAction func onLogInButton(_ sender: Any) {
+        
+        let myUrl = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: myUrl, success: {
+            
+            UserDefaults.standard.set( true, forKey: "userLoggedIn")
+            
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+            
+        }, failure: { (Error) in
+            print("Couldn't Log In")
+        }
+        )
+    }
+    
+
+}

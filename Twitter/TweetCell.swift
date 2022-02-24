f1ea24eccaa08141a54d92274561eb9760d3879a
riikacharya-code <racharya@vassar.edu>
2022-02-24 18:04:06 -0500

    twitter_ios_starter

--- Twitter/TweetCell.swift
+++ Twitter/TweetCell.swift
@@ -25,4 +25,14 @@ class TweetCell: UITableViewCell {
         // Configure the view for the selected state
     }
 
+    var favorited: Bool = false
+    
+    func setFavorite(_ isFavorited:Bool){
+        favorited = isFavorited
+        
+        if (favorited) {
+            favButton.set
+        }
+    }
+    
 }
---
+++ Twitter/TweetViewController.swift
@@ -0,0 +1,43 @@
+//
+//  TweetViewController.swift
+//  Twitter
+//
+//  Created by Riik Acharya on 2/21/22.
+//  Copyright © 2022 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetViewController: UIViewController {
+
+    @IBOutlet weak var tweetTextView: UITextView!
+    
+    @IBAction func tweetAction(_ sender: Any) {
+        if (!tweetTextView.text.isEmpty){
+            TwitterAPICaller.client?.postTweet(tweetString: tweetTextView.text, success: {
+                self.dismiss(animated: true, completion: nil)
+                
+            }, failure: {
+                (error) in print("Error posting tweet \(error)")
+            })
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tweetTextView.becomeFirstResponder()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
